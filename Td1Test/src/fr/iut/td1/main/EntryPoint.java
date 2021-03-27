6a041c06c85f8ab02bfb4bb6c566bfff49352bf2
night <night4deadgaming@gmail.com>
2021-03-27 10:12:33 +0100

    HELLO

--- Td1Test/src/fr/iut/td1/main/EntryPoint.java
+++ Td1Test/src/fr/iut/td1/main/EntryPoint.java
@@ -9,18 +9,18 @@ public class EntryPoint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD ABDOUL COMMENT QU'C'EST");
 		System.out.println("-----------------------------");
-		// Création du bilan :
+		// Crï¿½ation du bilan :
 		Bilan bilan = new Bilan();
 		bilan.setNum(1);
 		System.out.println("-----------------------------");
-		// Création du graphe :
+		// Crï¿½ation du graphe :
 		IObserver graphe = new UIWindowsGraphe(1);
 		IObserver graphe2 = new UIPhoneGraphe(2);
 		IObserver graphe3 = new UIWindowsGraphe(3);
 		// Je rejoute mon graphe en tant qu'observateur ...
-		// Bilan est observé par UIGraphe :
+		// Bilan est observï¿½ par UIGraphe :
 		bilan.addObserver(graphe);
 		bilan.addObserver(graphe2);
 		bilan.addObserver(graphe3);

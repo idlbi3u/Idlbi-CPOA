49e862c39a84653b93dda612d88df211cf083876
Abdoul Alim <abdoul-alim.idlbi5@etu.univ-lorraine.fr>
2021-04-17 11:12:37 +0200

    Last version

--- TdTestUnitaire/src/main/test_math.java
+++ TdTestUnitaire/src/main/test_math.java
@@ -7,7 +7,7 @@ public class test_math {
 
 	public static void main(String[] args){
 		IMaths maths = new Maths();
-		System.out.println("Multiplication : 4 × 5 = "+maths.multiplicationParAddition(4, 5));
+		System.out.println("Multiplication : 4 × 5 = "+maths.multiplicationParAddition(4, 1));
 	}
 
 }
